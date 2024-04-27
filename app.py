e51170d048392f26050bdfa9b95ad529d32eb922
JosueJs99 <2100499@aluno.univesp.br>
2024-04-27 14:06:55 -0300

    mudança para graficos de relações

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import dash
-from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 import fiona
 import geopandas as gpd
+import plotly.graph_objects as go
+from dash import Dash, html, dcc, Input, Output
 from shapely.geometry import Point, Polygon, mapping
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from statistics import mean
+from plotly.subplots import make_subplots
 
 #Carregando os dados e gerando o data frame 
 dfdengue = pd.read_csv("dengue_1-14.csv")
@@ -25,7 +27,8 @@ mp_bairro = gpd.read_file('BAIRROS+GUARAREMA.geojson')
 
 # Adicionando coluna casos de dengue
 
-dengue = [4, 0, 2, 2, 2, 5, 4, 5, 4, 0, 4, 0, 2, 2, 2, 2, 4, 4, 4, 4, 5, 5, 4, 0, 2, 0, 5, 0, 4, 2, 2, 4, 0, 2, 0, 0, 2, 4, 2]
+dengue = [4, 0, 2, 2, 2, 5, 4, 5, 4, 0, 4, 0, 2, 2, 2, 2, 4, 4, 4, 4,
+          5, 5, 4, 0, 2, 0, 5, 0, 4, 2, 2, 4, 0, 2, 0, 0, 2, 4, 2]
 
 # Transformar casos em index
 
@@ -51,10 +54,49 @@ fig_map = px.choropleth_mapbox(mp_bairro,
 fig_map.update_layout(margin=dict(l=1, r=1, t=1, b=1))
 
 # Gerando os graficos a serem exibidos
-    
-fig_cas = px.line(dfcasos, x='Data', y="Casos", markers=True)
-fig_temp = px.line(dfcasos, x='Data', y="Temperatura", markers=True, title='Temperatura')
-fig_umid = px.line(dfcasos, x='Data', y="Umidade", markers=True, title='Umidade')
+
+# Gráfico relação entre Dengue e Temperatura
+
+fig_dvst = make_subplots(specs=[[{"secondary_y": True}]])
+# Casos de dengue
+fig_dvst.add_trace(go.Scatter(x = dfcasos['Data'], y = dfcasos["Casos"], name = 'Casos de Dengue'))
+# Temperatura
+fig_dvst.add_trace(go.Scatter(x = dfcasos['Data'], y = dfcasos["Temperatura"], name = 'Temperatura Média'),
+              secondary_y=True
+              )
+
+fig_dvst.update_layout(title='Relação entre Dengue e Temperatura',
+xaxis_title='Semanas',
+yaxis_title='Casos',
+plot_bgcolor = 'white',
+font = {'family': 'Arial','size': 16,'color': 'black'},
+colorway=px.colors.qualitative.Set1)
+fig_dvst.update_xaxes( showgrid=True, gridwidth=1, gridcolor='lightgray',
+showline=True, linewidth=1, linecolor='black')
+fig_dvst.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgray',
+showline=True, linewidth=1, linecolor='black')
+
+# Gráfico relação entre Dengue e Umidade
+
+
+fig_dvsu = make_subplots(specs=[[{"secondary_y": True}]])
+# Casos de dengue
+fig_dvsu.add_trace(go.Scatter(x = dfcasos['Data'], y = dfcasos["Casos"], name = 'Casos de Dengue'))
+# Umidade
+fig_dvsu.add_trace(go.Scatter(x = dfcasos['Data'], y = dfcasos["Umidade"], name = 'Umidade média'),
+             secondary_y=True
+              )
+
+fig_dvsu.update_layout(title='Relação entre Dengue e Umidade',
+xaxis_title='Semanas',
+yaxis_title='Casos',
+plot_bgcolor = 'white',
+font = {'family': 'Arial','size': 16,'color': 'black'},
+colorway=px.colors.qualitative.Light24)
+fig_dvsu.update_xaxes( showgrid=True, gridwidth=1, gridcolor='lightgray',
+showline=True, linewidth=1, linecolor='black')
+fig_dvsu.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgray',
+showline=True, linewidth=1, linecolor='black')
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -68,8 +110,8 @@ app.layout = dbc.Container(
         html.Hr(),
                dbc.Tabs(
             [
-                dbc.Tab(label="Casos de Dengue", tab_id="casos"),
-                dbc.Tab(label="Clima", tab_id="clima"),
+                dbc.Tab(label="Dengue e Temperatura", tab_id="d_vs_t"),
+                dbc.Tab(label="Dengue e Umidade", tab_id="d_vs_u"),
                 dbc.Tab(label="Mapa de casos", tab_id="mapa")
             ],
             id="tabs",
@@ -86,15 +128,10 @@ app.layout = dbc.Container(
 def render_tab_content(active_tab, data):
 
     # Definindo abas ativas
-    if active_tab == "casos":
-        return dcc.Graph(figure=fig_cas)
-    elif active_tab == "clima":
-        return dbc.Row(
-            [
-                dbc.Col(dcc.Graph(figure=fig_temp)),
-                dbc.Col(dcc.Graph(figure= fig_umid)),
-            ]
-        )
+    if active_tab == "d_vs_t":
+        return dcc.Graph(figure=fig_dvst)
+    elif active_tab == "d_vs_u":
+        return dcc.Graph(figure= fig_dvsu)
     elif active_tab == "mapa":
         return dcc.Graph(figure=fig_map)
     
